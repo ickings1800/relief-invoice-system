38ad74a352fe23c08f01a4ad8df275d9c3a930cb
Ray <limlehao@gmail.com>
2019-12-27 14:33:34 +0800

    remove html2canvas and jspdf functionality

--- relief/pos/static/pos/js/trip_detail.js
+++ relief/pos/static/pos/js/trip_detail.js
@@ -811,14 +811,3 @@ function getCustomerId(customerName){
     }
     return customerId;
 }
-
-function print(){
-    console.log('print pdf');
-    const filename = 'invoice.pdf';
-    let options = { pagesplit: true,'background': '#fff' };
-    html2canvas(document.querySelector('#print-page')).then(canvas => {
-        let pdf= new jsPDF('p', 'mm', 'a4');
-        pdf.addImage(canvas.toDataURL('image/png'), 'PNG', 0, 0, 211, 298);
-        pdf.save(filename);
-    });
-}
