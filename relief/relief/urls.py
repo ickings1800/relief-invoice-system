71a336824c1a405ceefd724e7f859f870ba00663
Ray <limlehao@gmail.com>
2020-06-07 13:34:27 +0800

    configure django debug toolbar url

--- relief/relief/urls.py
+++ relief/relief/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 from django.views.generic import RedirectView
 
@@ -23,3 +24,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('', RedirectView.as_view(pattern_name='pos:login')),
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
